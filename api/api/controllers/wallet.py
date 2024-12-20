a438191fd386d3fc51d429d8e71fadbe6acab6b0
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-12-20 18:37:05 +0100

    save wallets in database after creation

--- api/api/controllers/wallet.py
+++ api/api/controllers/wallet.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, jsonify
 
 from api.services.XrpLedger import XrpLedger
+from api.services.db_services import add_wallet_address
 
 wallet_routes = Blueprint('wallet_routes', __name__)
 
 @wallet_routes.route('/wallet', methods=['POST'])
 def create_wallet():
     xrp_ledger = XrpLedger()
-    return xrp_ledger.create_wallet()
+    wallet = xrp_ledger.create_wallet()
+    if "error" in add_wallet_address(wallet["account_data"]["Account"]):
+        return jsonify({"error": "Error the wallet address"}), 500
+    return wallet
 
 @wallet_routes.route('/wallet/<string:wallet>', methods=['GET'])
 def retrieve_wallet(wallet):
